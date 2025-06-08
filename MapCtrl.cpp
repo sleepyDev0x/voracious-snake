3cf9d0db4a556e1c8318da9ff77e4a30c664e507
sleepyDev0x <cuihsgg@qq.com>
2025-06-08 21:16:00 +0800

    renderer snake、food and map

---
+++ MapCtrl.cpp
@@ -0,0 +1,5 @@
+//
+// Created by cuihs on 2025/6/8.
+//
+
+#include "MapCtrl.h"
---
+++ MapCtrl.h
@@ -0,0 +1,16 @@
+//
+// Created by cuihs on 2025/6/8.
+//
+
+#ifndef MAPCTRL_H
+#define MAPCTRL_H
+
+
+
+class MapCtrl {
+
+};
+
+
+
+#endif //MAPCTRL_H
